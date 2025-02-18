8a0f47dfbe63e6eec37cab97993309bd89ae7cf1
Ben Amos <flashguardian13@gmail.com>
2025-02-18 12:36:01 -0800

    added verbosity to setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,6 +17,7 @@ fi
 # augeas-libs: for CertBot
 # httpd: Apache, for serving files to the web
 # python3: for CertBot via pip
+echo "Installing packages ..."
 dnf update -y
 dnf install -y \
     augeas-libs \
@@ -24,28 +25,34 @@ dnf install -y \
     python3
 
 # Run Apache and always run Apache
+echo "Starting Apache ..."
 systemctl start httpd
 systemctl enable httpd
 
 # Go to repository (checked out via user data script)
+echo "Updating webserver repository ..."
 cd ~/louve-website-v2
 git remote update
 git checkout main
 git pull --ff-only
 
 # Install webserver files
+echo "Installing webserver files ..."
 rsync -aq var/www/ /var/www
 
 # Give apache group access to the webserver files
+echo "Updating webserver file permissions ..."
 chown -R ec2-user:apache /var/www
 chmod 2775 /var/www
 find /var/www -type d -exec chmod 2775 {} \;
 find /var/www -type f -exec chmod 0664 {} \;
 
 # Add ec2-user to apache group
+echo "Adding ec2-user to apache group ..."
 usermod -a -G apache ec2-user
 
 # Install Certbot via pip for HTTPS
+echo "Installing CertBot via pip ..."
 dnf remove certbot
 python3 -m venv /opt/certbot/
 source /opt/certbot/bin/activate
@@ -55,15 +62,20 @@ pip install \
     certbot-apache
 
 # Install CertBot shim
+echo "Installing CertBot shim ..."
 cp scripts/certbot /usr/bin/
 
 # Install SSL Certs and enable HTTPS for Apache using Certbot
+echo "Enabling SSL via CertBot ..."
 certbot --apache -n -m '$1' --agree-tos
 
 # Set up automatic cert renewal
 # https://unix.stackexchange.com/a/744641/245955
+echo "Setting up automatic SSL cert renewal ..."
 cp systemd/system/certbot.service /lib/systemd/system/
 cp systemd/system/certbot.timer /lib/systemd/system/
 chmod 644 /lib/systemd/system/certbot.*
 systemctl enable --now certbot.timer
+
+echo "Setup complete!"
 
